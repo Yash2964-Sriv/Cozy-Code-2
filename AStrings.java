d899094b26d8caa9a4b17e916711c561741c2dc7
Yash Srivastava <yashsrivastava84170@gmail.com>
2026-03-13 22:06:53 +0530

    Initial Commit

---
+++ AStrings.java
@@ -0,0 +1,36 @@
+
+/**
+ * Basic DSA Problems - Add Strings (LeetCode 415)
+ * Add two numbers represented as strings (no BigInteger)
+ */
+public class AStrings {
+
+    // Problem: num1, num2 as strings. Return sum as string.
+
+    /**
+     * Add from right to left with carry.
+     * Time O(m+n), Space O(max(m,n))
+     */
+    public static String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int a = i >= 0 ? num1.charAt(i--) - '0' : 0;
+            int b = j >= 0 ? num2.charAt(j--) - '0' : 0;
+            int sum = a + b + carry;
+            sb.append(sum % 10);
+            carry = sum / 10;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=".repeat(60));
+        System.out.println("Problem: Add Strings");
+        System.out.println("=".repeat(60));
+
+        System.out.println("11 + 123 -> " + addStrings("11", "123"));
+        System.out.println("456 + 77 -> " + addStrings("456", "77"));
+    }
+}
