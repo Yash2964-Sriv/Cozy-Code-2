89d7610dfd11971187255cdab7027fa0d9d94ff4
Yash Srivastava <yashsrivastava84170@gmail.com>
2026-07-09 00:01:52 +0530

    Test after Windows reinstall

--- SmallestNumInInfiniteSeries.cpp
+++ SmallestNumInInfiniteSeries.cpp
@@ -1,26 +1,40 @@
-#include <set>
+/**
+ * Basic DSA - Smallest Number in Infinite Set (LeetCode 2336)
+ * Starts with all positive integers. popSmallest takes the minimum; addBack reinserts a removed value. Min-heap for returns plus a sequential cursor.
+ */
+#include <iostream>
+#include <queue>
+#include <vector>
 
-class SmallestInfiniteSet {
-private:
-    int nextSmallest = 1;
-    std::set<int> addedBack;
-
-public:
-    SmallestInfiniteSet() = default;
+struct SmallestInfiniteSet {
+    int cursor = 1;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> returned;
 
     int popSmallest() {
-        if (!addedBack.empty()) {
-            const int smallest = *addedBack.begin();
-            addedBack.erase(addedBack.begin());
-            return smallest;
+        if (!returned.empty() && returned.top() < cursor) {
+            int x = returned.top();
+            returned.pop();
+            return x;
         }
-
-        return nextSmallest++;
+        return cursor++;
     }
 
     void addBack(int num) {
-        if (num < nextSmallest) {
-            addedBack.insert(num);
-        }
+        if (num < cursor)
+            returned.push(num);
     }
 };
+
+int main() {
+    std::cout << std::string(60, '=') << "\n";
+    std::cout << "Problem: Smallest Number in Infinite Set\n";
+    std::cout << std::string(60, '=') << "\n";
+
+    SmallestInfiniteSet s;
+    std::cout << "popSmallest -> " << s.popSmallest() << "\n";
+    std::cout << "popSmallest -> " << s.popSmallest() << "\n";
+    s.addBack(1);
+    std::cout << "popSmallest -> " << s.popSmallest() << "\n";
+    std::cout << "popSmallest -> " << s.popSmallest() << "\n";
+    std::cout << "popSmallest -> " << s.popSmallest() << "\n";
+}
