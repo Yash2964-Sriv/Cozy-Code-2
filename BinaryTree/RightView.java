6bfe56c9a6406c5c2373084905f4ebefc37012e3
Yash Srivastava <yashsrivastava84170@gmail.com>
2026-08-17 23:34:42 +0530

    Test after Windows reinstall

---
+++ BinaryTree/RightView.java
@@ -0,0 +1,29 @@
+package BinaryTree;
+
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+    }
+}
+public class RightView {
+    class Solution {
+    ArrayList<Integer> rightView(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        view(root,0,ans);
+        return ans;
+    }
+
+    private void view(Node root, int level, ArrayList<Integer> ans) {
+        if(root==null) return;
+        if(level>=ans.size()) ans.add(root.data);
+        else ans.set(level,root.data);
+        view(root.left,level+1,ans);
+        view(root.right,level+1,ans);
+    }
+}
+}
