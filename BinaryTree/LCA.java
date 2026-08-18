04ec11ecd4105148d685aa8c776239afde660cbb
Yash Srivastava <yashsrivastava84170@gmail.com>
2026-08-18 23:59:17 +0530

    Test after Windows reinstall

---
+++ BinaryTree/LCA.java
@@ -0,0 +1,12 @@
+package BinaryTree;
+
+public class LCA {
+    Node lca(Node root, int p, int q) { // TC = O(n)
+        if(root==null) return null;
+        if(root.data==p || root.data==q) return root;
+        Node l = lca(root.left,p,q);
+        Node r = lca(root.right,p,q);
+        if(l!=null && r!=null) return root;
+        return (l==null) ? r : l;
+    }
+}
